9cfdcda1f951762d12bb17d5b545766b4dd1b06d
LucasSSLV <lukinhas.kell@hotmail.com>
2023-11-13 18:00:58 -0300

    adiciona tailwindCSS e inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./components/Form";
+import "./index.css";
+
 function App() {
   return (
-    <>
+    <div className='divCentral'>
       <Form />
-    </>
+    </div>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
+  const [name, setName]=useState();
+  const [sobrenome, setSobrenome]=useState();
+  const [email,setEmail] = useState();
+  const [senha, setSenha] = useState();
+
   return (
-    <div>Form</div>
+    <div>
+      <h1>
+      Preencha o Formulário
+      </h1>
+      <form className=''>
+        Nome:<br />
+        <input type="text" name="name" placeholder="Primeiro nome" value={name}/><br/>
+        Sobrenome:<br />
+        <input type="text" name="sobrenome" placeholder="Sobrenome" value={sobrenome}/><br/>
+        Email:<br />
+        <input type="email" name="email" placeholder="Email" value={email}/><br /> 
+        Senha:<br />
+        <input type="password" name="senha" placeholder="Senha" value={senha}/><br/>
+      </form>
+      </div>
   )
 }
