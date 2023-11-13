4982d4016a8670f780d39960eab309c9c4134879
LucasSSLV <lukinhas.kell@hotmail.com>
2023-11-13 19:09:01 -0300

    adiciona lógica se salvamento de dados no localStorage

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,88 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Form() {
-  const [name, setName]=useState();
-  const [sobrenome, setSobrenome]=useState();
-  const [email,setEmail] = useState();
-  const [senha, setSenha] = useState();
+  const [name, setName] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "sobrenome":
+        setSobrenome(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "senha":
+        setSenha(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClick = () => {
+    if (!name || !sobrenome || !email || !senha) {
+      return alert("Preencha todos os campos!");
+    }
+    localStorage.setItem("name", name);
+    localStorage.setItem("sobrenome", sobrenome);
+    localStorage.setItem("email", email);
+    localStorage.setItem("senha", senha);
+    return alert("Seus dados foram enviados com sucesso!");
+  };
 
   return (
     <div>
-      <h1>
-      Preencha o Formulário
-      </h1>
-      <form className=''>
-        Nome:<br />
-        <input type="text" name="name" placeholder="Primeiro nome" value={name}/><br/>
-        Sobrenome:<br />
-        <input type="text" name="sobrenome" placeholder="Sobrenome" value={sobrenome}/><br/>
-        Email:<br />
-        <input type="email" name="email" placeholder="Email" value={email}/><br /> 
-        Senha:<br />
-        <input type="password" name="senha" placeholder="Senha" value={senha}/><br/>
+      <h1>Preencha o Formulário</h1>
+      <form>
+        Nome:
+        <br />
+        <input
+          type="text"
+          name="name"
+          placeholder="Primeiro nome"
+          value={name}
+          onChange={handleChange}
+        />
+        <br />
+        Sobrenome:
+        <br />
+        <input
+          type="text"
+          name="sobrenome"
+          placeholder="Sobrenome"
+          value={sobrenome}
+          onChange={handleChange}
+        />
+        <br />
+        Email:
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <br />
+        Senha:
+        <br />
+        <input
+          type="password"
+          name="senha"
+          placeholder="Senha"
+          value={senha}
+          onChange={handleChange}
+        />
+        <br />
       </form>
-      </div>
-  )
+      <button onClick={handleClick}>Cadastrar</button>
+    </div>
+  );
 }
